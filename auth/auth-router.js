3e6a98106a98e45217b4c66ec1d0a8e7ddaaad26
Eric Richards <esholundr@yahoo.com>
2019-12-20 10:10:40 -0700

    wrote register endpoint and created authenticate-middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,14 +2,26 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const Users = require('../')
+const Users = require('../users/users-model');
+const secrets = require('../config/secrets');
 
+// endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+
 });
 
 module.exports = router;
